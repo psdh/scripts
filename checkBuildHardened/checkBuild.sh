20f6147c4a052218c7050c7658db80edf4d7f7d9
Prabhjyot Singh Sodhi <prabhjyotsingh95@gmail.com>
2015-03-20 22:12:42 +0530

    Descriptive comments

--- checkBuildHardened/checkBuild.sh
+++ checkBuildHardened/checkBuild.sh
@@ -1,17 +1,19 @@
-fedpkg srpm
+# The mock commands will be replaced by koji build commands, and when that is done, I'll directly start by putting the "%undefine ..." in the spec file and build. (that is I'll skip the initial build)
+
+fedpkg srpm         # make src.rpm file
 out=`ls *.src.rpm`
-mock -r fedora-rawhide-x86_64 $out
+mock -r fedora-rawhide-x86_64 $out  # to be replaced by koji build
 if [ "$?" -eq 0 ]
 then
     echo "[CHECKBUILD.SH] Ran Correctly with hardening! No problem with x86_64 architecture"
 else
     spec=`ls *.spec`
-    python disableHardened.py $spec
+    python disableHardened.py $spec         # essentially puts the %undefine _hardened_build into the spec file
     fedpkg srpm
-    mock -r fedora-rawhide-x86_64 $out
+    mock -r fedora-rawhide-x86_64 $out      # to be replaced by koji build
     if [ "$?" -eq 0 ]
     then
-        echo "[Checkbuild.sh] Ran correctly after disabling hardening of the build, this implies that this is build failure is due to hardening!! [[ToDo]]"
+        echo "[CHECKBUILD.SH] Ran correctly after disabling hardening of the build, this implies that this is build failure is due to hardening!! [[ToDo]]"
     else
         echo "[CHECKBUILD.SH] Does not run even after disabling hardening! Report to package owner"
     fi
